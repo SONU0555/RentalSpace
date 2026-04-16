99b41602dc9cb20a719d4d03c38057466ccff66a
Sonu kumar <ssonukumar.in@gmail.com>
2026-04-16 10:01:39 +0530

    Initial commit - RentSpace Backend

---
+++ rentspace/src/test/java/RentSpace/RentspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package RentSpace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
