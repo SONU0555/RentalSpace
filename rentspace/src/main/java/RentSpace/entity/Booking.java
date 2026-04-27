1d013e5ed77babd73cc99deef408b88aa0f309ff
Sonu kumar <ssonukumar.in@gmail.com>
2026-04-27 21:37:17 +0530

    create booking entity class

---
+++ rentspace/src/main/java/RentSpace/entity/Booking.java
@@ -0,0 +1,102 @@
+package RentSpace.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity(name = "bookings")
+public class Booking {
+    
+    public enum BookingStatus{
+        PENDING,
+        ACTIVE,
+        COMPLETED,
+        CANCELLED;
+    }
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    @ManyToOne
+    @JoinColumn(name = "tenant_id")
+    private User tenant;
+    
+    @ManyToOne
+    @JoinColumn(name = "property_id")
+    private Property property;
+    
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private BigDecimal rentAmount;
+    
+    @Enumerated(EnumType.STRING)
+    private BookingStatus status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getTenant() {
+        return tenant;
+    }
+
+    public void setTenant(User tenant) {
+        this.tenant = tenant;
+    }
+
+    public Property getProperty() {
+        return property;
+    }
+
+    public void setProperty(Property property) {
+        this.property = property;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public BigDecimal getRentAmount() {
+        return rentAmount;
+    }
+
+    public void setRentAmount(BigDecimal rentAmount) {
+        this.rentAmount = rentAmount;
+    }
+
+    public BookingStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(BookingStatus status) {
+        this.status = status;
+    }
+    
+    
+    
+
+}
