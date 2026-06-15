3dc13622054eda779e23619677d0a5115c959d9c
Sonu kumar <ssonukumar.in@gmail.com>
2026-06-15 15:36:03 +0530

    Initial commit

---
+++ src/test/java/rentalSpacePortfolio/RentalSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package rentalSpacePortfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
